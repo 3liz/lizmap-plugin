601ac5387c4146cb554888e6d9ca2022f44a713f
Michael Douchin <mdouchin@3liz.com>
2014-09-16 10:28:41 +0200

    Prepare release for 1.9.9

--- __init__.py
+++ __init__.py
@@ -53,7 +53,7 @@ def description():
     return "Publication plugin for Lizmap web application, by 3liz.com"
 
 def version():
-    return "Version 1.9.8"
+    return "Version 1.9.9"
 
 def icon():
     return "icon.png"
