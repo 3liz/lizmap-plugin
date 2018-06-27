1cce159f94b17faa694ab33b4d9a11da5a5b4386
David Marteau <david@innophi.com>
2018-06-27 09:53:28 +0200

    Remove python2 support packages

--- plugin_upload.py
+++ plugin_upload.py
@@ -1,7 +1,3 @@
-from __future__ import print_function
-from future import standard_library
-standard_library.install_aliases()
-from builtins import input
 #!/usr/bin/env python
 # This script uploads a plugin package on the server
 #
