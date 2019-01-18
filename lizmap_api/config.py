77a4434982e7d31283bbc593948b3b7cddfc61fb
Michael Douchin <mdouchin@3liz.com>
2019-01-18 18:04:17 +0100

    [FEATURE] auto add the link from metadataUrl in QGIS (derived from 06db59fbe1c768d3eff674eb8a8e7b195799deb9)

--- lizmap_api/config.py
+++ lizmap_api/config.py
@@ -260,7 +260,7 @@ class LizmapConfig:
             'wType': 'textarea', 'type': 'string', 'default': '', 'isMetadata':True
         },
         'link': {
-            'wType': 'text', 'type': 'string', 'default': ''
+            'wType': 'text', 'type': 'string', 'default': '', 'isMetadata':True
         },
         'minScale': {
             'wType': 'text', 'type': 'integer', 'default': 1
