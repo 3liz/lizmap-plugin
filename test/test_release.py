771399d26efcd50c521c5da04fecfd233b2068a0
Denis Rouzaud <denis.rouzaud@gmail.com>
2019-07-05 15:16:28 +0200

    do not create release all the time

--- test/test_release.py
+++ test/test_release.py
@@ -4,7 +4,6 @@ import os
 import unittest
 import yaml
 from github import Github
-import git
 
 from qgispluginci.parameters import Parameters
 from qgispluginci.release import release
@@ -13,7 +12,7 @@ from qgispluginci.exceptions import GithubReleaseNotFound
 from github import GithubException
 
 # if change, also update on .travis.yml
-RELEASE_VERSION_TEST = '9.8.7'
+RELEASE_VERSION_TEST = '0.1.2'
 
 
 class TestRelease(unittest.TestCase):
@@ -25,9 +24,8 @@ class TestRelease(unittest.TestCase):
         self.github_token = os.getenv('github_token')
         self.repo = None
         if self.github_token:
-            print('creating fake release on Github ({})'.format(RELEASE_VERSION_TEST))
+            print('init Github')
             self.repo = Github(self.github_token).get_repo('opengisch/qgis-plugin-ci')
-            self.repo.create_git_release(RELEASE_VERSION_TEST, 'some fake release for testing', '', draft=False, prerelease=False)
 
     def tearDown(self):
         if self.repo:
@@ -37,15 +35,10 @@ class TestRelease(unittest.TestCase):
             except GithubException as e:
                 raise GithubReleaseNotFound('Release {} not found'.format(RELEASE_VERSION_TEST))
             if rel:
-                print('cleaning release')
-                rel.delete_release()
-                git_repo = git.repo.Repo()
-                git_repo.remotes.origin.fetch()
-                for t in git_repo.tags:
-                    if t.name == RELEASE_VERSION_TEST:
-                        print('cleaning tag {}'.format(t.name))
-                        git_repo.delete_tag(t)
-                        git_repo.remotes.origin.push(':{}'.format(t.path))
+                print('deleting release assets')
+                for asset in rel.get_assets():
+                    print('  delete {}'.format(asset.name))
+                    asset.delete_asset()
 
     def test_release(self):
         release(self.parameters, RELEASE_VERSION_TEST)
