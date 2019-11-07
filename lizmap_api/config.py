77a02d60ff7b614bea5bc35f6fc952b37e73caf7
Etienne Trimaille <gustrimaille@yahoo.fr>
2019-11-07 16:26:53 +0100

    start multiatlas

--- lizmap_api/config.py
+++ lizmap_api/config.py
@@ -43,13 +43,14 @@
 
  ***** END LICENSE BLOCK ***** */
 """
-from qgis.core import (QgsProject,
-                       QgsMapLayer)
 
-# import other needed tool
 import os
 import json
 import collections
+
+from qgis.core import (
+    QgsProject, QgsMapLayer,
+)
 
 
 class LizmapConfigError(Exception):
@@ -206,36 +207,6 @@ class LizmapConfig:
         },
         'datavizTemplate': {
             'wType': 'html', 'type': 'string', 'default': ''
-        },
-        'atlasEnabled': {
-            'wType': 'checkbox', 'type': 'boolean', 'default': False
-        },
-        'atlasLayer': {
-            'wType': 'layers', 'type': 'layer', 'default': '', 'list': []
-        },
-        'atlasPrimaryKey': {
-            'wType': 'fields', 'type': 'field', 'default': ''
-        },
-        'atlasDisplayLayerDescription': {
-            'wType': 'checkbox', 'type': 'boolean', 'default': True
-        },
-        'atlasFeatureLabel': {
-            'wType': 'fields', 'type': 'field', 'default': ''
-        },
-        'atlasSortField': {
-            'wType': 'fields', 'type': 'field', 'default': ''
-        },
-        'atlasHighlightGeometry': {
-            'wType': 'checkbox', 'type': 'boolean', 'default': True
-        },
-        'atlasZoom': {
-            'wType': 'list', 'type': 'string', 'default': 'zoom', 'list': ['', 'zoom', 'center']
-        },
-        'atlasDisplayPopup': {
-            'wType': 'checkbox', 'type': 'boolean', 'default': True
-        },
-        'atlasTriggerFilter': {
-            'wType': 'checkbox', 'type': 'boolean', 'default': False
         },
         'atlasShowAtStartup': {
             'wType': 'checkbox', 'type': 'boolean', 'default': False
