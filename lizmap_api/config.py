caa7adf843470d1f96f11e6f72fa668c137b031b
Michael Douchin <mdouchin@3liz.com>
2019-12-19 13:26:46 +0100

    Timemanager - Add endAttribute and resolution for upcoming LWC 3.4

--- lizmap_api/config.py
+++ lizmap_api/config.py
@@ -344,6 +344,14 @@ class LizmapConfig:
         }
     }
 
+
+    timemanagerOptionDefinitions = {
+        'attributeResolution': {
+            'wType': 'list', 'type': 'string', 'default': 'years',
+            'list': ['seconds', 'minutes', 'hours', 'days', 'weeks', 'months', 'year']
+        }
+    }
+
     datavizOptionDefinitions = {
         'plotType': {
             'wType': 'list', 'type': 'string', 'default': 'scatter',
