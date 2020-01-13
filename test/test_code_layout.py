bc51f3b0d321565d1a1d4438914be8762a1a79e5
Etienne Trimaille <gustrimaille@yahoo.fr>
2020-01-13 09:42:03 +0100

    fix 29c52b538bce709cb33b226a7e80249fc45aa0c7 wrong import #93

---
+++ test/test_code_layout.py
@@ -0,0 +1,35 @@
+"""Test code layout."""
+
+import os
+import re
+import unittest
+
+__copyright__ = 'Copyright 2020, 3Liz'
+__license__ = 'GPL version 3'
+__email__ = 'info@3liz.org'
+__revision__ = '$Format:%H$'
+
+
+class TestCodeLayout(unittest.TestCase):
+
+    def test_qgis_widgets(self):
+        """Test imports are correct in UI file."""
+        list_files = []
+        for root, directories, files in os.walk('../'):
+            for file in files:
+                if file.lower().endswith('.ui'):
+                    file_path = os.path.join(root, file)
+                    with open(file_path, 'r') as ui_file:
+                        matches = re.findall(
+                            r'<header>qgs[a-z]*\.h<\/header>',
+                            ui_file.read(),
+                            re.MULTILINE)
+                        if len(matches):
+                            list_files.append(file)
+                            print(matches)
+
+        self.assertEqual(
+            len(list_files),
+            0,
+            'Some imports are wrong : {}'.format(', '.join(list_files))
+        )
