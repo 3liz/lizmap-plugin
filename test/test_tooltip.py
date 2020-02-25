52798dd9afdf7a62c66a7485db3aa01ffe484ccb
Etienne Trimaille <gustrimaille@yahoo.fr>
2020-02-25 22:45:33 +0100

    more code layout in tootltips, add tests

--- test/test_tooltip.py
+++ test/test_tooltip.py
@@ -3,7 +3,13 @@ from qgis.core import (
     QgsVectorLayer,
     QgsProject,
     QgsEditFormConfig,
-    QgsExpression, QgsFeature, QgsExpressionContext, QgsExpressionContextUtils)
+    QgsExpression,
+    QgsFeature,
+    QgsExpressionContext,
+    QgsExpressionContextUtils,
+    QgsAttributeEditorContainer,
+    QgsOptionalExpression,
+    QgsAttributeEditorField)
 from qgis.gui import QgsExternalResourceWidget
 from qgis.testing import unittest, start_app
 
@@ -22,11 +28,62 @@ __revision__ = '$Format:%H$'
 
 class TestToolTip(unittest.TestCase):
 
-    def setUp(self) -> None:
-        self.layer = QgsVectorLayer('None?field=field_a:string', 'table', 'memory')
+    def check_layer_context(self, field_value, expression, expected):
+        layer = QgsVectorLayer('None?field=field_a:string', 'table', 'memory')
 
-        self.context = QgsExpressionContext()
-        self.context.appendScope(QgsExpressionContextUtils.layerScope(self.layer))
+        feature = QgsFeature()
+        feature.setAttributes([field_value])
+
+        context = QgsExpressionContext()
+        context.appendScope(QgsExpressionContextUtils.layerScope(layer))
+        context.setFeature(feature)
+
+        expression = QgsExpression(expression)
+        self.assertFalse(expression.hasParserError())
+
+        expression.prepare(context)
+        self.assertFalse(expression.hasEvalError())
+        self.assertEqual(expected, expression.evaluate(context))
+
+    def test_field_name(self):
+        """Test we can generate the field name correctly."""
+        template = Tooltip._generate_field_name('field_a', 'Field Name', '\'foo\'')
+
+        expected = '''
+                    [% CASE
+                        WHEN "field_a" IS NOT NULL OR trim("field_a") != ''
+                        THEN concat(
+                            '<p>', '<b>Field Name</b>',
+                            '<div class="field">', 'foo', '</div>',
+                            '</p>'
+                        )
+                        ELSE ''
+                    END %]'''
+        self.assertEqual(expected, template)
+
+        layer = QgsVectorLayer('None?field=field_a:string', 'table', 'memory')
+        sub_context = QgsExpressionContext()
+        sub_context.appendScopes(QgsExpressionContextUtils.globalProjectLayerScopes(layer))
+        feature = QgsFeature()
+        feature.setAttributes(['a'])
+        layer.dataProvider().addFeatures([feature])
+        sub_context.setFeature(next(layer.getFeatures()))
+        expression = QgsExpression().replaceExpressionText(template, sub_context)
+        self.assertEqual('\n                    <p><b>Field Name</b><div class="field">foo</div></p>', expression)
+
+    def test_relation_reference(self):
+        """Test we can generate a relation reference."""
+        result = Tooltip._generate_relation_reference('name', 'parent_pk', 'layer_id', 'display_expression')
+        expected = '''
+                    aggregate(
+                        layer:='layer_id',
+                        aggregate:='concatenate',
+                        expression:=display_expression,
+                        filter:=
+                    "parent_pk" = attribute(@parent, 'name')
+                
+                    )'''
+        self.assertEqual(expected, result)
 
     def test_value_map(self):
         """Test we can generate value map."""
@@ -41,57 +98,35 @@ class TestToolTip(unittest.TestCase):
                 }
             ]
         }
-        result = Tooltip._generate_value_map(widget_config, 'field_a')
+        expression = Tooltip._generate_value_map(widget_config, 'field_a')
         expected = '''
                     map_get(
                         hstore_to_map('"a"=>"A","b"=>"B"'),
                         "field_a"
                     )'''
-        self.assertEqual(expected, result)
-
-        feature = QgsFeature()
-        feature.setAttributes(['a'])
-        self.layer.dataProvider().addFeatures([feature])
-
-        expression = QgsExpression(result)
-        self.assertFalse(expression.hasParserError())
-
-        expression.prepare(self.context)
-        self.context.setFeature(next(self.layer.getFeatures()))
-        self.assertFalse(expression.hasEvalError())
-        self.assertEqual(expression.evaluate(self.context), 'A')
+        self.assertEqual(expected, expression)
+        self.check_layer_context('a', expression, 'A')
 
     def test_date(self):
         """Test we can generate date."""
         widget_config = {
             'display_format': 'yyyy',
         }
-        result = Tooltip._generate_date(widget_config, 'field_a')
+        expression = Tooltip._generate_date(widget_config, 'field_a')
         expected = '''
                     format_date(
                         "field_a",
                         'yyyy'
                     )'''
-        self.assertEqual(result, expected)
-
-        feature = QgsFeature()
-        feature.setAttributes(['2012-05-15'])
-        self.layer.dataProvider().addFeatures([feature])
-
-        expression = QgsExpression(result)
-        self.assertFalse(expression.hasParserError())
-
-        expression.prepare(self.context)
-        self.context.setFeature(next(self.layer.getFeatures()))
-        self.assertFalse(expression.hasEvalError())
-        self.assertEqual(expression.evaluate(self.context), '2012')
+        self.assertEqual(expected, expression)
+        self.check_layer_context('2012-05-15', expression, '2012')
 
     def test_external_resource_web(self):
         """Test we can generate external resource for web."""
         widget_config = {
             'DocumentViewer': QgsExternalResourceWidget.Web,
         }
-        result = Tooltip._generate_external_resource(widget_config, 'field_a', 'fname')
+        expression = Tooltip._generate_external_resource(widget_config, 'field_a', 'fname')
         expected = '''
                     concat(
                        '<a href="',
@@ -105,32 +140,20 @@ class TestToolTip(unittest.TestCase):
                        '
                        </a>'
                     )'''
-        self.assertEqual(result, expected)
+        self.assertEqual(expression, expected)
 
-        feature = QgsFeature()
-        feature.setAttributes(['test.pdf'])
-        self.layer.dataProvider().addFeatures([feature])
-
-        expression = QgsExpression(result)
-        self.assertFalse(expression.hasParserError())
-
-        expression.prepare(self.context)
-        self.context.setFeature(next(self.layer.getFeatures()))
-        self.assertFalse(expression.hasEvalError())
-        expected = (
-            '<a href="test.pdf" target="_blank">                                              '
-            '<iframe src="test.pdf" width="100%" height="300" title="fname"/>                       '
-            '</a>')
-        result = expression.evaluate(self.context)
-        result = result.replace('\n', '').replace('\r', '')
-        self.assertEqual(expected, result)
+        expected = '''<a href="test.pdf" target="_blank">
+                       
+                       <iframe src="test.pdf" width="100%" height="300" title="fname"/>
+                       </a>'''
+        self.check_layer_context('test.pdf', expression, expected)
 
     def test_external_resource_image(self):
         """Test we can generate external resource for an image."""
         widget_config = {
             'DocumentViewer': QgsExternalResourceWidget.Image,
         }
-        result = Tooltip._generate_external_resource(widget_config, 'field_a', 'fname')
+        expression = Tooltip._generate_external_resource(widget_config, 'field_a', 'fname')
         expected = '''
                     concat(
                        '<a href="',
@@ -143,54 +166,29 @@ class TestToolTip(unittest.TestCase):
                        '
                        </a>'
                     )'''
-        self.assertEqual(result, expected)
+        self.assertEqual(expression, expected)
 
-        feature = QgsFeature()
-        feature.setAttributes(['test.png'])
-        self.layer.dataProvider().addFeatures([feature])
-
-        expression = QgsExpression(result)
-        self.assertFalse(expression.hasParserError())
-
-        expression.prepare(self.context)
-        self.context.setFeature(next(self.layer.getFeatures()))
-        self.assertFalse(expression.hasEvalError())
-        expected = (
-            '<a href="test.png" target="_blank">                       '
-            '<img src="test.png" width="100%" title="fname">                       '
-            '</a>')
-        result = expression.evaluate(self.context)
-        result = result.replace('\n', '').replace('\r', '')
-        self.assertEqual(expected, result)
+        expected = '''<a href="test.png" target="_blank">
+                       <img src="test.png" width="100%" title="fname">
+                       </a>'''
+        self.check_layer_context('test.png', expression, expected)
 
     def test_external_resource_no_content(self):
         """Test we can generate external resource for no content."""
         widget_config = {
             'DocumentViewer': QgsExternalResourceWidget.NoContent,
         }
-        result = Tooltip._generate_external_resource(widget_config, 'field_a', 'fname')
+        expression = Tooltip._generate_external_resource(widget_config, 'field_a', 'fname')
         expected = '''
                     concat(
                         '<a href="',
                         "field_a",
                         '" target="_blank">fname</a>'
                     )'''
-        self.assertEqual(expected, result)
+        self.assertEqual(expected, expression)
 
-        feature = QgsFeature()
-        feature.setAttributes(['test.png'])
-        self.layer.dataProvider().addFeatures([feature])
-
-        expression = QgsExpression(result)
-        self.assertFalse(expression.hasParserError())
-
-        expression.prepare(self.context)
-        self.context.setFeature(next(self.layer.getFeatures()))
-        self.assertFalse(expression.hasEvalError())
         expected = '<a href="test.png" target="_blank">fname</a>'
-        result = expression.evaluate(self.context)
-        result = result.replace('\n', '').replace('\r', '')
-        self.assertEqual(expected, result)
+        self.check_layer_context('test.png', expression, expected)
 
     def test_value_relation(self):
         """Test we can generate a value relation."""
@@ -225,6 +223,96 @@ class TestToolTip(unittest.TestCase):
         expression = QgsExpression(result)
         self.assertFalse(expression.hasParserError())
 
+    def test_layer_non_existing_field(self):
+        """Test we do not generate for non existing fields."""
+        layer = QgsVectorLayer('None?field=field_a:string', 'table', 'memory')
+
+        feature = QgsFeature()
+        feature.setAttributes(['a'])
+        layer.dataProvider().addFeatures([feature])
+
+        config = layer.editFormConfig()
+        config.setLayout(QgsEditFormConfig.TabLayout)
+        config.clearTabs()
+
+        root = config.invisibleRootContainer()
+
+        tab_1 = QgsAttributeEditorContainer('tab1', root)
+        config.addTab(tab_1)
+
+        html_content = Tooltip.create_popup_node_item_from_form(
+            layer, root, 0, [], '', QgsProject.instance().relationManager())
+
+        expected = '''<ul class="nav nav-tabs">
+
+    <li class="active"><a href="#popup_dd_tab1" data-toggle="tab">tab1</a></li>
+</ul>
+<div class="tab-content">
+  <div id="popup_dd_tab1" class="tab-pane active">
+  </div>
+</div>'''
+        self.assertEqual(expected, html_content)
+
+        tab_1.addChildElement(QgsAttributeEditorField('fake_field', -1, tab_1))
+
+        html_content = Tooltip.create_popup_node_item_from_form(
+            layer, root, 0, [], '', QgsProject.instance().relationManager())
+
+        self.assertEqual(expected, html_content)
+
+    @unittest.skip
+    def test_display_group(self):
+        """Test we can hide some groups."""
+        layer = QgsVectorLayer('None?field=field_a:string', 'table', 'memory')
+
+        config = layer.editFormConfig()
+        config.setLayout(QgsEditFormConfig.TabLayout)
+        config.clearTabs()
+
+        root = config.invisibleRootContainer()
+
+        tab_1 = QgsAttributeEditorContainer('tab1', root)
+        config.addTab(tab_1)
+
+        tab_2 = QgsAttributeEditorContainer('tab2', root)
+        config.addTab(tab_2)
+
+        html_content = Tooltip.create_popup_node_item_from_form(
+            layer, root, 0, [], '', QgsProject.instance().relationManager())
+
+        expected = '''<ul class="nav nav-tabs">
+
+    <li class="active"><a href="#popup_dd_tab1" data-toggle="tab">tab1</a></li>
+
+    <li class=""><a href="#popup_dd_tab2" data-toggle="tab">tab2</a></li>
+</ul>
+<div class="tab-content">
+  <div id="popup_dd_tab1" class="tab-pane active">
+  </div>
+
+  <div id="popup_dd_tab2" class="tab-pane ">
+  </div>
+</div>'''
+        self.assertEqual(expected, html_content)
+
+        tab_1.setVisibilityExpression(QgsOptionalExpression(QgsExpression('True')))
+        tab_2.setVisibilityExpression(QgsOptionalExpression(QgsExpression('False')))
+
+        expected = '''<ul class="nav nav-tabs">
+
+        <li class="active"><a href="#popup_dd_tab1" data-toggle="tab">tab1</a></li>
+
+        <li class=""><a href="#popup_dd_tab2" data-toggle="tab">tab2</a></li>
+    </ul>
+    <div class="tab-content">
+      <div id="popup_dd_tab1" class="tab-pane active">
+      </div>
+
+      <div id="popup_dd_tab2" class="tab-pane ">
+      </div>
+    </div>'''
+        self.assertEqual(expected, html_content)
+
     def test_tooltip_layer(self):
         """Test tooltip on a layer."""
         layer = QgsVectorLayer(plugin_test_data_path('complex_form.geojson'), 'form', 'ogr')
@@ -239,8 +327,7 @@ class TestToolTip(unittest.TestCase):
             layer, root, 0, [], '', QgsProject.instance().relationManager())
 
         self.maxDiff = None
-        expected = '''
-<ul class="nav nav-tabs">
+        expected = '''<ul class="nav nav-tabs">
 
     <li class="active"><a href="#popup_dd_tab_1" data-toggle="tab">tab 1</a></li>
 
@@ -249,25 +336,23 @@ class TestToolTip(unittest.TestCase):
 <div class="tab-content">
   <div id="popup_dd_tab_1" class="tab-pane active">
     
-            [% CASE
-                WHEN "name" IS NOT NULL OR trim("name") != ''
-                THEN concat(
-                    '<p>', '<b>name</b>',
-                    '<div class="field">', 
+                    [% CASE
+                        WHEN "name" IS NOT NULL OR trim("name") != ''
+                        THEN concat(
+                            '<p>', '<b>name</b>',
+                            '<div class="field">', 
                     map_get(
                         hstore_to_map('"A"=>"a","B"=>"b","C"=>"c"'),
                         "name"
                     ), '</div>',
-                    '</p>'
-                )
-                ELSE ''
-            END %]
-            
+                            '</p>'
+                        )
+                        ELSE ''
+                    END %]
   </div>
 
   <div id="popup_dd_tab2" class="tab-pane ">
   </div>
-</div>
-'''.strip()
+</div>'''
 
         self.assertEqual(expected, html_content)
