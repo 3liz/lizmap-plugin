f1ba1c6e46dd677c039a9e235c511f6fe78de0f8
Etienne Trimaille <gustrimaille@yahoo.fr>
2020-02-27 16:35:19 +0100

    continue

--- lizmap/lizmap-locales/web-client/push_to_transifex.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-LOCALES_DIR="$(dirname $0)"
-
-source $LOCALES_DIR/module_list.sh
-
-tx push -s --branch $LOCALES_BRANCH
--- lizmap/qgis_plugin_tools/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Package for QGIS plugin utils."""
-
-__copyright__ = 'Copyright 2019, 3Liz'
-__license__ = 'GPL version 3'
-__email__ = 'info@3liz.org'
-__revision__ = '$Format:%H$'
