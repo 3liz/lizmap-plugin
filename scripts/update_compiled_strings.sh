cb662027d7e84f1b20fe389328bba39de7bf49cf
Etienne Trimaille <gustrimaille@yahoo.fr>
2020-04-03 18:58:31 +0200

    remove scripts folder

--- scripts/update_compiled_strings.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-LOCALES=$*
-
-for LOCALE in ${LOCALES}
-do
-    echo "Compiling lizmap-locales/plugin/i18n/lizmap_"${LOCALE}".ts"
-    lrelease lizmap-locales/plugin/i18n/lizmap_${LOCALE}.ts -qm lizmap-locales/plugin/i18n/lizmap_${LOCALE}.qm;
-done
