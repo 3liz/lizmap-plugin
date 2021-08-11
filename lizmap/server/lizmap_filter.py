76195e464e33312a26b0c128547a5776a2b1e7d3
Etienne Trimaille <gustrimaille@yahoo.fr>
2021-08-11 15:02:50 +0200

    Server - Move the exception in its own file

--- lizmap/server/lizmap_filter.py
+++ lizmap/server/lizmap_filter.py
@@ -8,40 +8,10 @@ import traceback
 
 from typing import List
 
-from qgis.PyQt.QtCore import QByteArray
-from qgis.PyQt.QtXml import QDomDocument
-from qgis.server import QgsServerException, QgsServerFilter, QgsServerInterface
+from qgis.server import QgsServerFilter, QgsServerInterface
 
+from lizmap.server.exception import LizmapFilterException
 from lizmap.server.logger import Logger
-
-
-class LizmapFilterException(QgsServerException):
-
-    def __init__(
-            self, code: str, message: str, locator: str = '', response_code: int = 500, version: str = '1.3.0') -> None:
-        super(QgsServerException, self).__init__(message, response_code)
-        self.code = code
-        self.message = message
-        self.locator = locator
-        self.response_code = response_code
-        self.version = version
-
-    def formatResponse(self) -> (QByteArray, str):
-        doc = QDomDocument()
-        root = doc.createElement('ServiceExceptionReport')
-        root.setAttribute('version', self.version)
-        root.setAttribute('xmlns', 'http://www.opengis.net/ogc')
-        doc.appendChild(root)
-
-        elem = doc.createElement('ServiceException')
-        elem.setAttribute('code', self.code)
-        elem.appendChild(doc.createTextNode(self.message))
-        root.appendChild(elem)
-
-        if self.locator:
-            elem.setAttribute('locator', self.locator)
-
-        return doc.toByteArray(), 'text/xml; charset=utf-8'
 
 
 class LizmapFilter(QgsServerFilter):
