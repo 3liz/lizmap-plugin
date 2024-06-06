ac70a09c4bdbd9253cb74c83fd5f74d781e2c394
Etienne Trimaille <gustrimaille@yahoo.fr>
2024-06-06 17:45:25 +0200

    Add LWC 3.9 in definitions.py file

--- lizmap/definitions/definitions.py
+++ lizmap/definitions/definitions.py
@@ -22,6 +22,7 @@ class LwcVersions(Enum):
     Lizmap_3_6 = '3.6'
     Lizmap_3_7 = '3.7'
     Lizmap_3_8 = '3.8'
+    Lizmap_3_9 = '3.9'
 
     def __lt__(self, other):
         if self.__class__ is other.__class__:
