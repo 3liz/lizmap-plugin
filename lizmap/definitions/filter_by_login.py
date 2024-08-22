75e30c11b2feb0e1870c93501f8dfbff57e2ab87
Etienne Trimaille <gustrimaille@yahoo.fr>
2024-08-22 17:28:17 +0200

    New option to allow multiple ACL values withing the same field

--- lizmap/definitions/filter_by_login.py
+++ lizmap/definitions/filter_by_login.py
@@ -1,13 +1,31 @@
 """Definitions for filter by login."""
+from enum import Enum, unique
 
 from lizmap.definitions.base import BaseDefinitions, InputType
 from lizmap.definitions.definitions import LwcVersions
 from lizmap.definitions.filter_by_polygon import FilterLogin
 from lizmap.toolbelt.i18n import tr
+from lizmap.toolbelt.resources import resources_path
 
-__copyright__ = 'Copyright 2020, 3Liz'
+__copyright__ = 'Copyright 2024, 3Liz'
 __license__ = 'GPL version 3'
 __email__ = 'info@3liz.org'
+
+
+@unique
+class SingleOrMultipleValues(Enum):
+    Multiple = {
+        'data': True,
+        'label': tr('comma separated list'),
+        'icon': resources_path('icons', 'user_group.svg'),
+        'tooltip': tr('The field can contain a list of values separated by a comma.')
+    }
+    Single = {
+        'data': False,
+        'label': tr('single value'),
+        'icon': resources_path('icons', 'user.svg'),
+        'tooltip': tr('The field can contain a single value.')
+    }
 
 
 class FilterByLoginDefinitions(BaseDefinitions):
@@ -40,6 +58,18 @@ class FilterByLoginDefinitions(BaseDefinitions):
             'default': False,
             'tooltip': tr('If this filter is used for edition capabilities only.'),
             'version': LwcVersions.Lizmap_3_4,
+        }
+        self._layer_config['allow_multiple_acl_values'] = {
+            'type': InputType.CheckBoxAsDropdown,
+            'header': tr('Allow multiple values'),
+            'items': SingleOrMultipleValues,
+            'default': SingleOrMultipleValues.Single.value['data'],
+            'tooltip': tr(
+                'If multiple values is allowed in the attribute table. '
+                'The multiple option is available only for PostgreSQL layer.'
+            ),
+            'version': LwcVersions.Lizmap_3_8,
+            'use_json': True,
         }
 
     @staticmethod
