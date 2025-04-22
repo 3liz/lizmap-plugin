b3a4a66b3170b2943147e2139c9897282c1ea0a4
Etienne Trimaille <gustrimaille@yahoo.fr>
2025-04-22 20:04:00 +0200

    Update qgis_plugin_tools for PyQt6

--- lizmap/definitions/lizmap_cloud.py
+++ lizmap/definitions/lizmap_cloud.py
@@ -21,7 +21,11 @@ UPLOAD_MAX_SIZE = 11000000  # 11 Mb
 EXCLUDED_DOMAINS = ('demo.snap.lizmap.com', 'demo.lizmap.com', 'localhost:8130', )
 # Domains which are designed for workshops
 # For the "Training" panel and excluded from Plausible as well
-WORKSHOP_DOMAINS = ('workshop.lizmap.com', 'formation.lizmap.com', )
+WORKSHOP_DOMAINS = (
+    'workshop.lizmap.com',
+    'formation.lizmap.com',
+    'openworkshop.fr',
+)
 
 # When the folder for storing QGS files is already created before the workshop
 WORKSHOP_FOLDER_ID = 'themeurbanism'
